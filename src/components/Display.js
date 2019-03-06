2841e8175935e7f7ee2ce1d5351ddb6258f5dfa6
joepound <14357797+joepound@users.noreply.github.com>
2019-03-06 13:55:03 -0700

    Added tests for Dashboard component

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,8 +4,8 @@ const Display = props => {
   return (
     <section className="Display">
       <h2 className="Display-header">Display</h2>
-      <h4>Balls: {props.balls}</h4>
-      <h4>Strikes: {props.strikes}</h4>
+      <h4 data-testId="balls-count">Balls: {props.balls}</h4>
+      <h4 data-testId="strikes-count">Strikes: {props.strikes}</h4>
     </section>
   );
 };
--- src/components/Display.test.js
+++ src/components/Display.test.js
@@ -5,14 +5,16 @@ import "jest-dom/extend-expect";
 
 import Display from "./Display";
 
-describe("Display component", () => {
+describe("Display component:", () => {
   it("• should render balls count", () => {
     const ballsText = render(<Display balls={3} />).getByText(/Balls: 3/);
     expect(ballsText).toBeInTheDocument();
   });
 
   it("• should render strikes count", () => {
-    const strikesText = render(<Display strikes={2} />).getByText(/Strikes: 2/i);
+    const strikesText = render(<Display strikes={2} />).getByText(
+      /Strikes: 2/i
+    );
     expect(strikesText).toBeInTheDocument();
   });
 });
